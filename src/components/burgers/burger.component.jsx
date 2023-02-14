de50fd0c20ecb9186dbfd36e88001dbe718cd6b2
Shakeel <shakeel.greyneon@gmail.com>
2023-02-14 21:05:47 +0500

    burger

--- src/components/burgers/burger.component.jsx
+++ src/components/burgers/burger.component.jsx
@@ -52,4 +52,37 @@ const mapDispatchToProps = dispatch => ({
 
 // export default Burger;
 
+
 export default connect(null, mapDispatchToProps)(Burger);
+
+
+
+{/* <div className="card" style={{width: "18rem"}}>
+<img src={image} className="card-img-top" alt="..."/>
+<img src='./Vector1.png' className='vector1' alt='..'/>
+<div className="card-body">
+  <h5 className="card-title">
+    <Link to={`/foods/${id}`}>{title}</Link>
+   </h5>
+  <p className="card-text">{description}</p> 
+  <p className='price'>${price}</p>
+  <button className='button' ><img src="./Vector.png" alt="" /></button>
+  <button className="addTOCart__btn" onClick={addItem} >
+          Add to Cart
+  </button>
+ </div> 
+</div>
+  </div>
+)
+}
+const mapDispatchToProps = dispatch => ({
+addItem: item => dispatch(addItem(item))
+})
+
+// export default Burger;
+
+
+
+
+
+export default connect(null, mapDispatchToProps)(Burger); */}
